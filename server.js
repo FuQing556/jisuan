cbc58764a99b62179875978af72293f37fb7fb7c
符青 <16041415+fu_qing_ling@user.noreply.gitee.com>
2026-07-04 14:12:53 +0800

    feat: add Node.js server for Railway deployment

---
+++ server.js
@@ -0,0 +1,22 @@
+const http = require('http');
+const fs   = require('fs');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'combination-finder.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end('Internal Server Error');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    res.end(data);
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Combination Finder running on port ${PORT}`);
+});
